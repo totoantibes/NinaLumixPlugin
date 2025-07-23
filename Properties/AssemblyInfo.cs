2f52b97a0abe39ec951b412b6ac929215bcd6deb
robert hasson <robert_hasson@yahoo.com>
2025-07-23 15:13:34 +0200

    logo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/totoantibes/NinaLumixPlugin")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
-[assembly: AssemblyMetadata("FeaturedImageURL", "https://github.com/totoantibes/NinaLumixPlugin/blob/stable/Lumix_logo.png")]
+[assembly: AssemblyMetadata("FeaturedImageURL", "https://github.com/totoantibes/NinaLumixPlugin/blob/nightly/LumixIconGPTsmall.png")]
 //[Optional] A url to an example screenshot of your plugin in action
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 //[Optional] An additional url to an example example screenshot of your plugin in action
